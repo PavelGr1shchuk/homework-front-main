a3005de57a74074dde62ae85a239f79657a70740
PavelGr1shchuk <arthurvlasow@gmail.com>
2025-03-26 00:38:10 +0300

    fix: usersList

--- src/HW/1/UserList.tsx
+++ src/HW/1/UserList.tsx
@@ -26,8 +26,7 @@ export const UserList = (props: UserListPropsType) => {
          <strong>{user.name}</strong>
          (Age: {user.age})
          <strong> Address:</strong>
-        {user.address.street},
-         {user.address.city}
+        {user.address.street}, {user.address.city}
           </li>
         ))}
     </ul>
